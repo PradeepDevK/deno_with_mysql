f15307fb136c84db57abd3244e323fb558ad5262
PradeepDevK <pradeep.rajk@yahoo.com>
2020-07-12 19:48:33 +0530

    Error fix

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 const dbCreds = {
     user: "root",
-    database: "codeforgeek_test",
-    password: "",
+    database: "deno_test",
+    password: "root",
     hostname: "127.0.0.1",
     port: 3306,
     poolsize: 3,
